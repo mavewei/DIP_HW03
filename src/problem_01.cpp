ad74410b02b2295f86e280cec769a0c830b3b2bf
mavewei <mavewei@gmail.com>
2013-11-03 16:29:09 +0800

    Modification on labelComponent()

--- src/problem_01.cpp
+++ src/problem_01.cpp
@@ -18,7 +18,8 @@
  void erode(unsigned char *image, unsigned char *erodeImage);
  void boundaryExtract(unsigned char *image, unsigned char *erodeImage);
  void componentCount(int x, int y, unsigned char *image, bool *visitImage, unsigned char pixelValue); 
- void labelComponent(unsigned char *image, unsigned char *labelImage);
+ // void labelComponent(unsigned char *image, unsigned char *labelImage);
+ void labelComponent(int x, int y, unsigned char *image, int pixel, unsigned char pixelValue);
  void binaryImage(unsigned char *image, unsigned char *binaryImage);
  int writeImagePGM(char *filename, int height, int width, unsigned char *image);
 
@@ -77,7 +78,21 @@
         }
         
         if(VERBOSE) printf("  --LABELING CONNECTED COMPONENT WITH DIF. COLORS--\n");
-        labelComponent(imgBin, imgOut);
+        for(int x = 0; x < HEIGHT; x++) {
+            for(int y = 0; y < WIDTH; y++) {
+                imgOut[x * WIDTH +y] = imgBin[x * WIDTH + y];
+            }
+        }
+
+        int color = 1;
+        for(int x = 0; x < HEIGHT; x++) {
+            for(int y = 0; y < WIDTH; y++) {
+                    labelComponent(x, y, imgOut, color, imgBin[x * WIDTH + y]);
+                    color += 15;
+            }
+        }
+
+        // labelComponent(imgBin, imgOut);
         if(VERBOSE) printf("     *Writting output image to L.pgm\n");
         sprintf(outputFilename, "L.pgm");
         if(writeImagePGM(outputFilename, HEIGHT, WIDTH, imgOut) == 0) {
@@ -120,56 +135,69 @@ void componentCount(int x, int y, unsigned char *image, bool *visitImage, unsign
     }
 }
 
-void labelComponent(unsigned char *image, unsigned char *labelImage) {
-    int x, y, numberCircle = 0, labelID;
-
-    for(x = 0; x < HEIGHT; x++) {
-        for(y = 0; y < WIDTH; y++) {
-            labelImage[x * WIDTH + y] = image[x * WIDTH + y];
-        }
-    }
-
-    labelID = 35;
-
-    for(x = 0; x < HEIGHT; x++) {
-        for (y = 0; y < WIDTH; y++) {
-            if(image[x * WIDTH + y] == WHITE) {
-                if(x - 1 >= 0 && y - 1 >= 0) {
-                    if(image[x * WIDTH + (y - 1)] == 0 && image[(x - 1) * WIDTH + y] == 0) {
-                        labelImage[x * WIDTH + y] = labelID;
-                        labelID++;
-                    } else {
-                        if(image[x * WIDTH + (y - 1)] == 255) {
-                            labelImage[x * WIDTH + y] = labelImage[x * WIDTH + (y - 1)];
-                        } 
-                        else 
-                        {
-                            labelImage[x * WIDTH + y] = labelImage[(x - 1) * WIDTH + y];
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    for(x = 0; x < HEIGHT; x++) {
-        for (y = 0; y < WIDTH; y++) {
-            if(image[x * WIDTH + y] == WHITE) {
-                int m, n, min = 255;
-                for(m = -1; m < 2; m++){
-                    for (n = -1; n < 2; n++) {
-                        if((x + m >= 0) && ((x + m) < HEIGHT) && (y + n >= 0) && ((y + n) < WIDTH) && image[(x + m) * WIDTH + (y + n)] != BLACK) {
-                            if(labelImage[(x + m) * WIDTH + (y + n)] < min) {
-                                min = labelImage[(x + m) * WIDTH + (y + n)];
-                            }
-                        }
-                    }
-                }
-                labelImage[x * WIDTH + y] = min;
-            }
+void labelComponent(int x, int y, unsigned char *image, int pixel, unsigned char pixelValue) {
+    if(x >= 0 && x < HEIGHT && y >= 0 && y < WIDTH) {
+        if (image[x * WIDTH + y] == pixelValue) 
+        {
+           image[x * WIDTH + y] = pixel;
+           labelComponent(x + 1, y, image, pixel, pixelValue);
+           labelComponent(x - 1, y, image, pixel, pixelValue);
+           labelComponent(x, y + 1, image, pixel, pixelValue);
+           labelComponent(x, y - 1, image, pixel, pixelValue);
         }
     }
 }
+
+// void labelComponent(unsigned char *image, unsigned char *labelImage) {
+//     int x, y, numberCircle = 0, labelID;
+
+//     for(x = 0; x < HEIGHT; x++) {
+//         for(y = 0; y < WIDTH; y++) {
+//             labelImage[x * WIDTH + y] = image[x * WIDTH + y];
+//         }
+//     }
+
+//     labelID = 35;
+
+//     for(x = 0; x < HEIGHT; x++) {
+//         for (y = 0; y < WIDTH; y++) {
+//             if(image[x * WIDTH + y] == WHITE) {
+//                 if(x - 1 >= 0 && y - 1 >= 0) {
+//                     if(image[x * WIDTH + (y - 1)] == 0 && image[(x - 1) * WIDTH + y] == 0) {
+//                         labelImage[x * WIDTH + y] = labelID;
+//                         labelID++;
+//                     } else {
+//                         if(image[x * WIDTH + (y - 1)] == 255) {
+//                             labelImage[x * WIDTH + y] = labelImage[x * WIDTH + (y - 1)];
+//                         } 
+//                         else 
+//                         {
+//                             labelImage[x * WIDTH + y] = labelImage[(x - 1) * WIDTH + y];
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//     }
+
+//     for(x = 0; x < HEIGHT; x++) {
+//         for (y = 0; y < WIDTH; y++) {
+//             if(image[x * WIDTH + y] == WHITE) {
+//                 int m, n, min = 255;
+//                 for(m = -1; m < 2; m++){
+//                     for (n = -1; n < 2; n++) {
+//                         if((x + m >= 0) && ((x + m) < HEIGHT) && (y + n >= 0) && ((y + n) < WIDTH) && image[(x + m) * WIDTH + (y + n)] != BLACK) {
+//                             if(labelImage[(x + m) * WIDTH + (y + n)] < min) {
+//                                 min = labelImage[(x + m) * WIDTH + (y + n)];
+//                             }
+//                         }
+//                     }
+//                 }
+//                 labelImage[x * WIDTH + y] = min;
+//             }
+//         }
+//     }
+// }
 
 void boundaryExtract(unsigned char *image, unsigned char *erodeImage) {
     int x, y;
